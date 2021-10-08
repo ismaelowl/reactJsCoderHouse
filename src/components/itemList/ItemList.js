cbccb79b93ec9f07a12d3d5fa999ff5e50f27835
Ismael Peralta <ismael.owl@hotmail.com>
2021-10-08 16:42:49 -0300

    Fix Aling Item in ItemList

--- src/components/itemList/ItemList.js
+++ src/components/itemList/ItemList.js
@@ -1,15 +1,16 @@
 import { Flex, Box, Heading } from '@chakra-ui/react';
 import Item from '../item/Item';
-
+import './itemList.css'
 const ItemList = ({ data, nombre }) => {
 
     return (
         <Box maxW="container.xl" mt="10">
             <Heading size="lg">{nombre}</Heading>
             <Flex
-                mt="5" flexWrap="wrap"
-                alignContent="center"
-                justifyContent="space-between">
+                className="item-container"
+                mt="5"
+                flexWrap="wrap"
+                alignContent="center">
                 {data.map((e) => (
                     <Item data={e} key={e.id}></Item>
                 ))}
